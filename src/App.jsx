2d4e297ba558d080fb5ce504da01af99ea6f56ec
unknown <shashwotpradhan@gmail.com>
2024-06-22 21:47:17 +0545

    1commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Typography } from '@mui/material'
+
+
+const App = () => {
+  return (
+    <div>
+      <Typography variant='h1'>App</Typography>
+    </div>
+  )
+}
+
+export default App
